26a68bf33526736697471539a0afbd6a51f54285
Rene Cocom <fasnimo@gmail.com>
2019-08-01 05:37:55 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
+  attr_accessor :name, :years
 
+  @@all = []
+
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip = 0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meals|
+      meals.waiter == self
+    end
+  end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_1, meal_2|
+      meal_1.tip <=> meal_2.tip
+    end
+    best_tipped_meal.customer
+  end
 end
